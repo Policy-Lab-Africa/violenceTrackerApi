d1fc16b2a10d8505085a1a11f62fb8ffbeb7cd79
Cornelius <cornelius.udeh@yahoo.com>
2023-02-02 23:32:11 +0100

    Return PU details with `violence_reports`

--- app/Http/Requests/StoreViolenceReportRequest.php
+++ app/Http/Requests/StoreViolenceReportRequest.php
@@ -24,13 +24,15 @@ class StoreViolenceReportRequest extends FormRequest
     public function rules()
     {
         return [
-            'ng_state_id' => 'nullable|exists:ng_states,data_id|integer',
+            'ng_state_id'   => 'nullable|exists:ng_states,data_id|integer',
             'ng_local_government_id' => 'nullable|exists:ng_local_governments,data_id|integer',
             'ng_polling_unit_id' => 'required|exists:ng_polling_units,data_id|integer',
-            'type_id' => 'required|exists:violence_types,id|integer',
-            'title' => 'nullable|string|min:5',
-            'description' => 'string|required_without:file_path|max:2500',
-            'file' => 'required_without:description|mimes:jpeg,png,jpg,mp4,mov,ogg,qt,m3u8,3gp,avi,wmv|max:20000',   
+            'type_id'       => 'required|exists:violence_types,id|integer',
+            'title'         => 'nullable|string|min:5',
+            'description'   => 'string|required_without:file_path|max:2500',
+            'file'          => 'required_without:description|mimes:jpeg,png,jpg,mp4,mov,ogg,qt,m3u8,3gp,avi,wmv|max:20000',
+            'longitude'     => 'nullable|string',
+            'latitude'      => 'nullable|string',
         ];
     }
 }
