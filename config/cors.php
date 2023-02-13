3ea9b0259f80487a6dab075f91c7e83d6cc66d29
Cornelius <cornelius.udeh@yahoo.com>
2023-02-13 15:20:02 +0100

    handle cors

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'OPTION', 'HEAD', 'POST', 'PUT', 'PATCH'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost', 'http://localhost:3000', 'https://violence-tracker-web-o7pr.vercel.app',],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
