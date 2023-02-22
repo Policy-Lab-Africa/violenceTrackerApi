0ed3eae29f9da61addbff860e72b448762799dd9
Cornelius <cornelius.udeh@yahoo.com>
2023-02-23 00:24:11 +0100

    chore: force CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\JsonResponse::class,
+            \App\Http\Middleware\ForceCors::class,
         ],
     ];
 
---
+++ app/Http/Middleware/ForceCors.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class ForceCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        
+        $response = $next($request);
+
+        if(App::environment(['local']))
+        {
+            $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Credentials', 'true');
+        } else {
+            $response
+            ->header('Access-Control-Allow-Origin', '*.violencetrack.ng')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Credentials', 'true');
+
+        }
+
+
+        return $response;
+    }
+}
