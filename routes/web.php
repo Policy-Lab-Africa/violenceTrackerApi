8811af9c0ad18beabe184d6c882ec595d5deee46
Cornelius <cornelius.udeh@yahoo.com>
2023-02-23 00:02:36 +0100

    chore: redirect to prod

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('app.violencetrack.ng');
+    return redirect()->away(url('https://app.violencetrack.ng'));
 });
